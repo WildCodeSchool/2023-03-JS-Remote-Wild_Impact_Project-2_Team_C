f4385ccc05c2e3be9695202be7694de71788ee7a
Lucas DEGRET <lucas.degret@outlook.fr>
2023-04-12 17:31:17 +0200

    Installation de Sass, ajout des variables dans Sass, ajout de nos logos dans le dossier 'assets', ajout des polices choisis, configuration par défaut du <body>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-import Page01 from "@pages/Page01";
-import Home from "@pages/Home";
 // US 1 import
-
+import Page01 from "@pages/Page01";
 // US 2 Import
 import Page02 from "@pages/Page02";
-
 // US 3 import
 import Page03 from "@pages/Page03";
 // US 4 import
@@ -12,22 +9,23 @@ import Page04 from "@pages/Page04";
 // US 5 Import
 import Page05 from "@pages/Page05";
 
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      {/** US 1 Page */}
-      <Page01 />
-      {/** US 2 Page */}
-      <Page02 />
-      {/** US 3 Page */}
-      <Page03 />
-      {/** US 4 Page */}
-      <Page04 />
-      {/* US 5 Page */}
-      <Page05 />
+      <body>
+        {/** US 1 Page */}
+        <Page01 />
+        {/** US 2 Page */}
+        <Page02 />
+        {/** US 3 Page */}
+        <Page03 />
+        {/** US 4 Page */}
+        <Page04 />
+        {/* US 5 Page */}
+        <Page05 />
+      </body>
     </div>
   );
 }

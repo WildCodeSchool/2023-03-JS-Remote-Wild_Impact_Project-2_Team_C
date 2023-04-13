0c0724e4a929adc1cd3d072f871028c2efa882c5
Lucas DEGRET <lucas.degret@outlook.fr>
2023-04-13 15:20:00 +0200

    Création du composant 'Banner' + son style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,31 @@
-// US 1 import
-import Page01 from "@pages/Page01";
-// US 2 Import
-import Page02 from "@pages/Page02";
-// US 3 import
-import Page03 from "@pages/Page03";
-// US 4 import
-import Page04 from "@pages/Page04";
-// US 5 Import
-import Page05 from "@pages/Page05";
+// // US 1 import
+// import Page01 from "@pages/Page01";
+// // US 2 Import
+// import Page02 from "@pages/Page02";
+// // US 3 import
+// import Page03 from "@pages/Page03";
+// // US 4 import
+// import Page04 from "@pages/Page04";
+// // US 5 Import
+// import Page05 from "@pages/Page05";
 
 import Banner from "@components/Banner";
-
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <body>
-        <Banner />
-        {/** US 1 Page */}
-        <Page01 />
-        {/** US 2 Page */}
-        <Page02 />
-        {/** US 3 Page */}
-        <Page03 />
-        {/** US 4 Page */}
-        <Page04 />
-        {/* US 5 Page */}
-        <Page05 />
-      </body>
+      <Banner />
+      {/** US 1 Page */}
+      {/* <Page01 /> */}
+      {/** US 2 Page */}
+      {/* <Page02 /> */}
+      {/** US 3 Page */}
+      {/* <Page03 /> */}
+      {/** US 4 Page */}
+      {/* <Page04 /> */}
+      {/* US 5 Page */}
+      {/* <Page05 /> */}
     </div>
   );
 }
--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Banner() {
-  return <div>Banner</div>;
+  return (
+    <div className="banner">
+      <img
+        src="../src/assets/logo_wildflix.png"
+        alt="logo_wildflix"
+        id="logo_wild"
+      />
+      <h1> WildFlix + </h1>
+      <img
+        src="../src/assets/logo_compte.png"
+        alt="logo_compte"
+        id="logo_compte"
+      />
+    </div>
+  );
 }
 
 export default Banner;

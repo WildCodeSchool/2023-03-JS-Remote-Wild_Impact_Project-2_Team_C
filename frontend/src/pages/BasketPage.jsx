716c46bdeb0c7c547761328801c2d0c85c48942f
Lucas DEGRET <lucas.degret@outlook.fr>
2023-04-14 12:07:54 +0200

    Finalisation de la première version de BasketPage

--- frontend/src/pages/BasketPage.jsx
+++ frontend/src/pages/BasketPage.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import Banner from "@components/Banner";
 import "../styles/BasketPage.scss";
-import { PropTypes } from "prop-types";
 
 function BasketPage({ film }) {
   return (
     <div>
       <Banner />
       <div className="basket-page">
-        <h1 className="basket-title"> Panier </h1>
-        <section> {film.poster_path} </section>
+        <h2 className="basket-title"> Panier </h2>
+        <section className="basket-container">
+          <img src={film.poster_path} alt={film.title} />
+          <div className="recap-container">
+            <h3> {film.title} </h3>
+            <p> price : {film.price} </p>
+          </div>
+        </section>
+      </div>
+      <button type="button" className="btn-paid">
+        Acheter
+      </button>
+      <div className="recommandation">
+        <h2> Ceux qui ont achetés ces produits ont pris aussi </h2>
+
+        <section className="reco-picture">
+          <img src={film.poster_path} alt={film.title} />
+          <img src={film.poster_path} alt={film.title} />
+          <img src={film.poster_path} alt={film.title} />
+          <img src={film.poster_path} alt={film.title} />
+          <img src={film.poster_path} alt={film.title} />
+          <img src={film.poster_path} alt={film.title} />
+        </section>
       </div>
     </div>
   );
 }
 
-BasketPage.propTypes = {
-  film: PropTypes.string.isRequired,
-};
 export default BasketPage;

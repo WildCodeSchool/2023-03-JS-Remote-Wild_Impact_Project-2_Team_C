e978cbe747d59d79e68ca4eb08290f101fe43181
Lucas DEGRET <lucas.degret@outlook.fr>
2023-04-14 10:15:16 +0200

    Création de la feuille 'BasketPage.scss' et import dans 'BasketPage.jsx'

--- frontend/src/pages/BasketPage.jsx
+++ frontend/src/pages/BasketPage.jsx
@@ -1,13 +1,15 @@
-import Banner from "@components/Banner";
 import React from "react";
+import Banner from "@components/Banner";
+import "../styles/BasketPage.scss";
+
 
 function BasketPage() {
   return (
     <div className="basket-page">
       <Banner />
-      <h1> Test </h1>
+      <h1 className="basket-title"> Test </h1>
     </div>
-  )
+  );
 }
 
 export default BasketPage;

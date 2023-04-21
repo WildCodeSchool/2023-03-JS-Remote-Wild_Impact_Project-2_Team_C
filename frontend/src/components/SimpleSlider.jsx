d58450a81185e277a8edc61f4218e6bb96cc711e
Robin31 <robindenis@hotmail.fr>
2023-04-21 12:28:53 +0200

    CSS terminé, Code terminé, svp acceptez moi la PR

--- frontend/src/components/SimpleSlider.jsx
+++ frontend/src/components/SimpleSlider.jsx
@@ -10,8 +10,8 @@ function SimpleSlider({ title, propriete, value }) {
     {
       breakpoint: 768,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 2,
+        slidesToShow: 2,
+        slidesToScroll: 1,
         infinite: true,
         dots: false,
       },
@@ -19,8 +19,8 @@ function SimpleSlider({ title, propriete, value }) {
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 4,
-        slidesToScroll: 3,
+        slidesToShow: 3,
+        slidesToScroll: 2,
         infinite: true,
         dots: false,
       },
@@ -35,8 +35,8 @@ function SimpleSlider({ title, propriete, value }) {
           dots={false}
           infinite
           speed={500}
-          slidesToShow={2}
-          slidesToScroll={1}
+          slidesToShow={4}
+          slidesToScroll={3}
           responsive={responsive}
         >
           {films

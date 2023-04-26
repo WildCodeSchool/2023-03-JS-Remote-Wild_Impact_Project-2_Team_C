1d74f03d8009d306c4ef6f56b6e7c191ceb69814
Camfabjeh <camille.fabry@yahoo.com>
2023-04-26 10:34:31 +0200

    ajout du 3ème film de la DB

--- backend/database.sql
+++ backend/database.sql
@@ -22,6 +22,9 @@ VALUES (false, "https://image.tmdb.org/t/p/w500/iS9U3VHpPEjTWnwmW56CrBlpgLj.jpg"
 INSERT INTO films (adult, backdrop_path, genre_ids, original_language, original_title, overview, popularity, poster_path, release_date, title, video, vote_average, vote_count, price) 
 VALUES (false, "https://image.tmdb.org/t/p/w500/5GA3vV1aWWHTSDO5eno8V5zDo8r.jpg", "Horror", "en", "Orphan: First Kill", "After escaping from an Estonian psychiatric facility, Leena Klammer travels to America by impersonating Esther, the missing daughter of a wealthy family. But when her mask starts to slip, she is put against a mother who will protect her family from the murderous “child” at any cost.", 6689.425, "https://image.tmdb.org/t/p/w500/pHkKbIRoCe7zIFvqan9LFSaQAde.jpg", "2022-07-27", "Orphan: First Kill", false, 6.9, 928, 8);
 
+INSERT INTO films (adult, backdrop_path, genre_ids, original_language, original_title, overview, popularity, poster_path, release_date, title, video, vote_average, vote_count, price) VALUES (false, "https://image.tmdb.org/t/p/w500/y2Ca1neKke2mGPMaHzlCNDVZqsK.jpg", "Action", "en", "Bullet Train", "Unlucky assassin Ladybug is determined to do his job peacefully after one too many gigs gone off the rails. Fate, however, may have other plans, as Ladybug's latest mission puts him on a collision course with lethal adversaries from around the globe—all with connected, yet conflicting, objectives—on the world's fastest train.", 5396.345, "https://image.tmdb.org/t/p/w500/tVxDe01Zy3kZqaZRNiXFGDICdZk.jpg", "2022-07-03", "Bullet Train", false, 7.5, 1592, 8);
+
+
 
 
 

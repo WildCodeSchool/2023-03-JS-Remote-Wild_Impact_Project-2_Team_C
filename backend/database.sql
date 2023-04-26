ca3bdcf15c342abcc32441ecb7e2fe6e3cc45fd0
Camfabjeh <camille.fabry@yahoo.com>
2023-04-26 10:13:28 +0200

    Création de la base de données avec la table films

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,20 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
+CREATE TABLE films (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  adult BOOLEAN NOT NULL,
+  backdrop_path VARCHAR(255) NOT NULL,
+  genre_ids VARCHAR(20) NOT NULL,
+  original_language VARCHAR(2) NOT NULL,
+  original_title VARCHAR(50) NOT NULL,
+  overview LONGTEXT NOT NULL,
+  popularity NUMERIC(4,3),
+  poster_path VARCHAR(255) NOT NULL,
+  release_date VARCHAR(10) NOT NULL,
+  title VARCHAR(50) NOT NULL,
+  video BOOLEAN NOT NULL,
+  vote_average DECIMAL(2,1),
+  vote_count INT,
+  price INT NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+ 
+

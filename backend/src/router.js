baacb2ed413d49e8539fc69ac731f5388e28a548
Camfabjeh <camille.fabry@yahoo.com>
2023-04-26 15:30:31 +0200

    mise à jour des pages déclarées (suppression BasketPage et WatchlistPage)

--- backend/src/router.js
+++ backend/src/router.js
@@ -13,12 +13,12 @@ database
     console.error(err);
   });
 
-// http://localhost:5000 => HomePage
+// http://localhost:5000 => HomePage un router.get/carrousel récupère params de catégories
 router.get("/", (req, res) => {
   res.status(200).send("Ceci est la HomePage");
 });
 
-// http://localhost:5000/films => SearchPage
+// http://localhost:5000/films => SearchPage ajouter params selon filtres
 router.get("/films", (req, res) => {
   res.status(200).send("Ceci est la SearchPage");
 });
@@ -28,16 +28,6 @@ router.get("/films/:id", (req, res) => {
   res
     .status(200)
     .send("Ceci est une page qui vous présente un film en particulier");
-});
-
-// localhost:5000/panier => BasketPage
-router.get("/panier", (req, res) => {
-  res.status(200).send("Bienvenue dans votre panier");
-});
-
-// localhost:5000/watchlist => WatchlistPage
-router.get("/watchlist", (req, res) => {
-  res.status(200).send("Page qui montre les films achetés");
 });
 
 module.exports = router;

a9a4ade21143268b9d7a4bcef7268de371e9ec5a
Lucas DEGRET <lucas.degret@outlook.fr>
2023-04-27 16:08:15 +0200

    Mise en place du component BasketContainer

--- frontend/src/components/BasketContainer.jsx
+++ frontend/src/components/BasketContainer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-function BasketContainer() {
-  return <div>BasketContainer</div>;
+function BasketContainer({ film }) {
+  return (
+    <section className="basket-container">
+      <img src={film.poster_path} alt={film.title} />
+      <div className="recap-container">
+        <h3> {film.title} </h3>
+        <p> price : {film.price} €</p>
+        <h4 className="basket-button"> Supprimer </h4>
+      </div>
+    </section>
+  );
 }
 
 export default BasketContainer;

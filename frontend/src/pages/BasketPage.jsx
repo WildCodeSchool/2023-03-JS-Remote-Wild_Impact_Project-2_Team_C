e067f22a6b8b55547850f1f422722082b0a64643
Lucas DEGRET <lucas.degret@outlook.fr>
2023-04-27 16:12:37 +0200

    Mise en place de la page BasketPage

--- frontend/src/pages/BasketPage.jsx
+++ frontend/src/pages/BasketPage.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import "../Style/BasketPage.scss";
+import BasketContainer from "../components/BasketContainer";
+import RecoPicture from "../components/RecoPicture";
+import films from "../data/films";
 
 function BasketPage() {
-  return <div>BasketPage</div>;
+  const [buy, setBuy] = useState("Buy");
+  return (
+    <div>
+      <div className="basket-page">
+        <h2 className="basket-title"> Panier </h2>
+        <div className="orga">
+          {films.slice(0, 3).map((film) => (
+            <BasketContainer film={film} />
+          ))}
+        </div>
+        <h2 className="basket-total">Total : X €</h2>
+        <button
+          type="button"
+          className="basket-button"
+          onClick={() => setBuy("Paid confirm, Thank you !")}
+        >
+          {buy}{" "}
+        </button>
+      </div>
+
+      <div className="recommandation">
+        <h2> Ceux qui ont acheté ces produits ont pris aussi </h2>
+        <section>
+          {films.slice(10, 19).map((film) => (
+            <RecoPicture film={film} />
+          ))}
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default BasketPage;

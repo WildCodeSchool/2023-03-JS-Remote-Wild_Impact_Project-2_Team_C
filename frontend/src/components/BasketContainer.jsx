b1cee521555da47adac0355d05538bd1db9a0343
Lucas DEGRET <126179037+MrCelsus@users.noreply.github.com>
2023-04-27 16:26:13 +0200

    Update BasketContainer.jsx
    Uniformisation

--- frontend/src/components/BasketContainer.jsx
+++ frontend/src/components/BasketContainer.jsx
@@ -4,10 +4,10 @@ function BasketContainer({ film }) {
   return (
     <section className="basket-container">
       <img src={film.poster_path} alt={film.title} />
-      <div className="recap-container">
+      <div className="basket-container-recap">
         <h3> {film.title} </h3>
         <p> price : {film.price} €</p>
-        <h4 className="basket-button"> Supprimer </h4>
+        <h4 className="basket-container-button"> Supprimer </h4>
       </div>
     </section>
   );

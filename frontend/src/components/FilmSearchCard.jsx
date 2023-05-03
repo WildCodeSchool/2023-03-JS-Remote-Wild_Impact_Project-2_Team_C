925591e9fc7846d90f268593d93f8393f8a5626b
Maillardjay <jay_10@hotmail.fr>
2023-05-03 16:17:08 +0200

    mise à jour des className du component Film SearchPage pour récupérer le SCSS de mon fichier style SearchPage

--- frontend/src/components/FilmSearchCard.jsx
+++ frontend/src/components/FilmSearchCard.jsx
@@ -2,8 +2,25 @@ import React from "react";
 
 function FilmSearchCard({ movie }) {
   return (
-    <div>
-      <h3>{movie.title}</h3>
+    <div className="search-card">
+      <div className="search-card-header">
+        <img
+          className="search-card-img"
+          src={movie.poster_path}
+          alt={movie.title}
+        />
+        <p className="search-card-Average" id="average">
+          {movie.vote_average}
+        </p>
+      </div>
+
+      <h2 className="search-card-title">{movie.title}</h2>
+
+      <div className="search-card-body">
+        <p className="search-card-Category">{movie.genre_ids}</p>
+        <p className="search-card-Date">{movie.release_date}</p>
+        <p className="search-card-Price">{movie.price}</p>
+      </div>
     </div>
   );
 }

41d687e342cfd054ddddcac3f4921eb244b7a052
tony3717 <gentytony@gmail.com>
2023-05-03 15:30:54 +0200

    relier avec email.js

--- frontend/src/pages/ContactForm.jsx
+++ frontend/src/pages/ContactForm.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import emailjs from "@emailjs/browser";
 import "../styles/ContactForm.scss";
 
 function ContactForm() {
@@ -7,7 +9,18 @@ function ContactForm() {
   const [message, setMessage] = useState("");
 
   const submit = () => {
-    alert("your message has been sent");
+    emailjs
+      .send(
+        "service_xwa6g6j",
+        "template_cb1lvcm",
+        { name, email, message },
+        "G_O_1quvRgbvoXSyA"
+      )
+      .then(() => {
+        // eslint-disable-next-line no-alert
+        alert("your message has been sent");
+      })
+      .catch((err) => console.error(err));
   };
 
   return (

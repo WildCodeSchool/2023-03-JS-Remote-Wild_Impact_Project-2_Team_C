d430d1589e4c0606ac3d9e2f67d2f18764242e0a
Lucas DEGRET <lucas.degret@outlook.fr>
2023-05-05 12:34:32 +0200

    Mise en place de la nouvelle route contact sur le composant Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "../styles/Navbar.scss";
 import HOME from "../assets/HOME.png";
 import LOUPE from "../assets/LOUPE.png";
-import CLAP from "../assets/CLAP.png";
+import CONTACT from "../assets/CONTACT.png";
 import BASKET from "../assets/BASKET.png";
 
 function Navbar() {
@@ -19,14 +19,14 @@ function Navbar() {
           <img src={LOUPE} alt={LOUPE} />
         </button>
       </Link>
-      <Link to="/Description">
-        <button type="button" className="NavbarClapBtn">
-          <img src={CLAP} alt={CLAP} />
-        </button>
-      </Link>
       <Link to="/Basket">
         <button type="button" className="NavbarBasketBtn">
           <img src={BASKET} alt={BASKET} />
+        </button>
+      </Link>
+      <Link to="/Contact">
+        <button type="button" className="NavbarContactBtn">
+          <img src={CONTACT} alt={CONTACT} />
         </button>
       </Link>
     </div>
